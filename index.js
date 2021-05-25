c2d70e5eb3eb72a971141282498ba38e7c4b4020
IanAndrade2003 <ianandrade90@outlook.com>
2021-05-25 12:05:32 -0300

    Final

---
+++ index.js
@@ -0,0 +1,10 @@
+const nome = "Ian";
+let idade = 18;
+let ano = 2021
+const humano = true
+let numero1 = 1
+let numero2 = -1
+
+console.log('Meu ano de nascimento é', ano -= idade)
+console.log('Você é humano ?', humano)
+console.log(numero1 += numero2)
